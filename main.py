2fcf26de225cff52171c5eeeb44193446714ade9
AlexMenor <alexmenor@github.com>
2020-02-27 13:02:42 +0100

    parte3

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ clases = data['target']
 
 nombre_clases = data['target_names']
 
-clases_y_sus_datos = [{'x': [], 'y': []}, {'x': [], 'y': []}, {'x': [], 'y': []}]
+clases_y_sus_datos = [{'x': [], 'y':[]}, {'x': [], 'y':[]}, {'x': [], 'y':[]}]
 
 for i in range(0, len(clases)):
     dict = clases_y_sus_datos[clases[i]]
@@ -19,16 +19,18 @@ for i in range(0, len(clases)):
 
 for i in range(0, len(nombre_clases)):
     clases_y_sus_datos[i]['label'] = nombre_clases[i]
-    clases_y_sus_datos[i]['color'] = (np.random.rand(), np.random.rand(), np.random.rand())
 
-fig, ax = plt.subplots()
+clases_y_sus_datos[0]['color'] = 'pink'
+clases_y_sus_datos[1]['color'] = 'blue'
+clases_y_sus_datos[2]['color'] = 'green'
+
+fig, (ax1, ax2) = plt.subplots(1,2)
 
 for clase in clases_y_sus_datos:
-    ax.scatter(clase['x'], clase['y'], label=clase['label'], color=clase['color'])
+    ax1.scatter(clase['x'], clase['y'], label=clase['label'], color=clase['color'])
 
-ax.legend()
-ax.grid(True)
-plt.show()
+ax1.legend()
+ax1.grid(True)
 
 
 from sklearn.model_selection import train_test_split
@@ -36,3 +38,24 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(caracteristicas_en_bruto, clases, test_size=0.2, train_size=0.8, stratify=clases)
 
 
+samples = np.linspace(0, 2 * np.pi, 100)
+
+sin_arr = []
+cos_arr = []
+sin_plus_cos_arr = []
+
+for sample in samples:
+    sin = np.sin(sample)
+    cos = np.cos(sample)
+    sin_arr.append(sin)
+    cos_arr.append(cos)
+    sin_plus_cos_arr.append(sin + cos)
+
+
+ax2.plot(samples, sin_arr, color="black", label="sin(x)")
+ax2.plot(samples, cos_arr, color="blue", label="cos(x)")
+ax2.plot(samples, sin_plus_cos_arr, color="red", label="sin(x) + cos(x)")
+ax2.legend()
+
+
+plt.show()
