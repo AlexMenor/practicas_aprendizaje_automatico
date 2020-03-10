4f943d7d4a824e26cc7c267546e768234589ef8e
Alex Menor <alex4menor@gmail.com>
2020-03-10 13:17:56 +0100

    avanzamos ejer2

--- p1/ejercicio2.py
+++ p1/ejercicio2.py
@@ -46,9 +46,9 @@ def sign(error):
 
 def Err(x,y,w):
 	error = 0
-	for i in range(x):
+	for i in range(len(x)):
 		guess = 0
-		for j in range(x[i]):
+		for j in range(len(x[i])):
 			guess += w[j] * x[i][j]
 
 		if sign(guess) != y[i]:
@@ -57,9 +57,39 @@ def Err(x,y,w):
 
 	return error / len(x)
 
+def h(x, w):
+	guess = 0
+	for i in range (len(x)):
+		guess += x[i]*w[i]
+	return guess
+
+# Gradiente Descendente
+def gd(x, y, eta):
+	w = np.array([0, 0, 0])
+	for i in range(1000):
+		for j in range(len(w)):
+			sum = 0
+			for n in range(len(x)):
+				guess = sign(h(x[n], w))
+				sum += (x[n][j] * (guess - y[n]))
+			w[j] -= eta * sum
+
+	return w
 # Gradiente Descendente Estocastico
-def sgd(x, y):
-	w = np.array([0,0,0])
+def sgd(x, y, eta, number_of_minibatches):
+	mini_batches_x = np.array_split(x, number_of_minibatches)
+	mini_batches_y = np.array_split(y, number_of_minibatches)
+	w = np.array([0, 0, 0])
+
+	for xs, ys in zip(mini_batches_x, mini_batches_y):
+		for j in range(len(w)):
+			sum = 0
+			for n in range(len(xs)):
+				guess = sign(h(xs[n], w))
+				sum += (xs[n][j] * (guess - ys[n]))
+			w[j] -= eta * sum
+
+	return w
 
 # Pseudoinversa	
 def pseudoinverse():
@@ -71,9 +101,19 @@ x, y = readData('datos/X_train.npy', 'datos/y_train.npy')
 # Lectura de los datos para el test
 x_test, y_test = readData('datos/X_test.npy', 'datos/y_test.npy')
 
-w = sgd(x, y)
+eta = 0.01
+w = gd(x, y, eta)
+
+print ('Bondad del resultado para grad. descendente :\n')
+print ("Ein: ", Err(x, y, w))
+print ("Eout: ", Err(x_test, y_test, w))
+
+eta = 0.01
+n_minibatches = 30
+w = sgd(x, y, eta, n_minibatches)
+
 print ('Bondad del resultado para grad. descendente estocastico:\n')
-print ("Ein: ", Err(x,y,w))
+print ("Ein: ", Err(x, y, w))
 print ("Eout: ", Err(x_test, y_test, w))
 
 input("\n--- Pulsar tecla para continuar ---\n")
